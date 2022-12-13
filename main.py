82f51c40f99bb7546e68e21ecac41a2078d5e54b
Raihana Zahra <raihanazahra04@gmail.com>
2022-12-13 13:35:50 -0800

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Collaborators: Raihana Zahra, Rayane Tarazi, Lauren Campbell
+About: main game loop that contains references to every other game file
+"""
+import pygame, random, os
+from gameplay import Game
+
+g = Game()
+
+while g.running:
+    g.music_player()
+    g.curr_menu.display_menu()
+    g.game_loop()
+
